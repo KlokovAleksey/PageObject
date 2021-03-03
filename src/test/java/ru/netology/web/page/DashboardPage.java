ad01e592195118e7e32ce9b946950e7250cd5ed2
Aleksey Klokov <lex-oz@yandex.ru>
2021-03-03 23:22:23 +0300

    psixanul,ydalil MoneyTransferTest

--- src/test/java/ru/netology/web/page/DashboardPage.java
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -1,6 +1,7 @@
 package ru.netology.web.page;
 
 import com.codeborne.selenide.SelenideElement;
+import lombok.val;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
@@ -15,6 +16,7 @@ public class DashboardPage {
   private SelenideElement firstCard = $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0'] .button");
   private SelenideElement secondCard = $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d'] .button");
 
+
   public TransferPage replenishFirstCard(){
     firstCard.click();
     return new TransferPage();
@@ -24,6 +26,7 @@ public class DashboardPage {
     secondCard.click();
     return new TransferPage();
   }
+
 
 }
 
